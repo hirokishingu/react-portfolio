92ff133955937421436a24f52991117ee6e2e293
hirokishingu <hrk.bass.526@gmail.com>
2019-11-22 10:55:32 +0900

    add header and main body.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
 import * as React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Button from '@material-ui/core/Button';
-import VerticalTabs from './components/vertical-tabs'
-import IconLabelTabs from './components/icon-label-tabs';
-import LinearDeterminate from './components/progress/linear-progress';
+import Header from './components/header'
+import MainBody from './components/main-body'
 
 export class App extends React.Component {
   render() {
     return (
       <div className="App" >
-        <VerticalTabs></VerticalTabs>
-        <IconLabelTabs />
-        <LinearDeterminate />
-        <Button variant="contained" color="primary">
-          test button
-        </Button>
+        <Header></Header>
+        <MainBody></MainBody>
       </div>
     );
   }
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import { App } from './App'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme();
+
+ReactDOM.render(
+  <MuiThemeProvider theme={theme}>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={App} />
+      </Switch>
+    </BrowserRouter>
+  </MuiThemeProvider>,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
