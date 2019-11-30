88c025acd1ac6cbfb3205d07eba21587fab5daff
hirokishingu <hrk.bass.526@gmail.com>
2019-11-30 18:05:30 +0900

    Add main-body.tsx..

--- src/components/main-body.tsx
+++ src/components/main-body.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import styled from 'styled-components';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import HrkSngMM from './hrksng-card.tsx/hrksng-card';
+import { Motion, spring, StaggeredMotion } from 'react-motion';
+import HrkSngSky from './sky/sky';
+import MediaCard from './cards/hrk-sng-card';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -19,6 +15,9 @@ const useStyles = makeStyles((theme: Theme) =>
     title: {
       flexGrow: 1,
     },
+    cardMargin: {
+      margin: 50,
+    }
   }),
 );
 
@@ -27,6 +26,28 @@ export default function MainBody() {
 
   return (
     <React.Fragment>
+      <Motion defaultStyle={{ x: 0 }} style={{ x: spring(10) }}>
+        {value => <div>{value.x}</div>}
+      </Motion>
+      <StaggeredMotion
+        defaultStyles={[{ h: 0 }, { h: 0 }, { h: 0 }]}
+        styles={prevInterpolatedStyles => prevInterpolatedStyles!.map((_, i) => {
+          return i === 0
+            ? { h: spring(100) }
+            : { h: spring(prevInterpolatedStyles![i - 1].h) }
+        })}>
+        {(interpolatingStyles: { map: (arg0: (style: any, i: any) => JSX.Element) => React.ReactNode; }) =>
+          <div>
+            {interpolatingStyles.map((style, i) =>
+              <div key={i} style={{ border: '1px solid', height: style.h }} />)
+            }
+          </div>
+        }
+      </StaggeredMotion>
+      <div className={classes.cardMargin}>
+        <MediaCard></MediaCard>
+      </div>
+      <HrkSngSky></HrkSngSky>
     </React.Fragment>
   );
 }
