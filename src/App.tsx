3f8b0f47484bed76ae2787f9c8a1802c01eed4e3
hirokishingu <hrk.bass.526@gmail.com>
2019-12-07 10:53:07 +0900

    Add modules.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import './App.css';
 import Header from './components/header'
 import MainBody from './components/main-body'
+import Example from './components/react-typicals/hello-world';
+import HelloWorld from './components/react-typicals/hello-world';
 
 export class App extends React.Component {
   render() {
